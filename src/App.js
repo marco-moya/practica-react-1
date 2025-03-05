5a924693d6e28930acfed48ec3020b3d4c2dd2ba
Marco Moya <zgmf_x235@hotmail.com>
2025-03-05 16:54:13 -0600

    Add practice react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Song from './components/Song';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    console.log("La app se ha cargado correctamente");
+  }
+  render() {
+    return (
+      <div className='container-song'>
+        <Header />
+        <Song song="Fireflies" artist="The Midnight Owls" album="Moonlit Memories" />
+        <Song song="Summer Breeze" artist="Ocean Waves" album="Coastal Serenity" />
+        <Song song="Starry Nights" artist="Aurora Skyline" album="Celestial Dreams" />
+      </div>
+    );
+  }
 }
 
 export default App;
