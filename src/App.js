9fd48f6667a9ec197f218e9780c6cdf8fb580c1f
Marco Moya <zgmf_x235@hotmail.com>
2025-03-19 20:13:48 -0600

    feat: implement pop-up modal with React Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, {useEffect, useState} from "react";
+import Modal from "react-modal";
 import Header from "./components/Header/Header";
 import SearchResults from "./components/SearchResults/SearchResults";
 import Library from "./components/Library/Library";
 import "./App.css";
+import "./modalStyles.css";
+
+Modal.setAppElement("#root");
 
 const App = () => {
   const songDataFake = [
@@ -10,13 +14,18 @@ const App = () => {
     {id:1, title:"Summer Breeze", artist:"Ocean Waves", album:"Coastal Serenity"},
     {id:2, title:"Starry Nights", artist:"Aurora Skyline", album:"Celestial Dreams"},
   ];
-  
+
   const [searchResults, setSearchResults] = useState(songDataFake);
   const [library, setLibrary] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   
   const addToLibrary = (song) => {
     if (!library.find(item => item.id === song.id)) {
       setLibrary([...library, song]);
+      setModalIsOpen(true);
+      setTimeout(() => {
+        setModalIsOpen(false);
+      }, 2000);
     }
   }
 
@@ -26,7 +35,15 @@ const App = () => {
 
   return (
     <>
-        <Header />
+      <Header />
+      <Modal
+      isOpen={modalIsOpen}
+      className="modal-content"
+      overlayClassName="modal-overlay"
+      contentLabel="Mensaje Temporal"
+      >
+        <p>La biblioteca se ha actualizado</p>
+      </Modal>
       <main className="container-song">
         <SearchResults songs={songDataFake} onAddToLibrary={addToLibrary}/>
         <Library songs={library} />
